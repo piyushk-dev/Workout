a3c5aab1b06f435a6deb3833598ffb7424465d97
Piyush Kumar <piyushkumar07999@gmail.com>
2025-06-20 12:23:30 +0530

    chore: deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,10 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+  const PORT = process.env.PORT || 443;
+  app.listen(PORT, "0.0.0.0", () =>
+    console.log(`Server running on port ${PORT}`)
+  );
   })
   .catch((error) => {
     console.log(error)
